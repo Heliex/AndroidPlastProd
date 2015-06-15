a0715108899e2f93c3f2da80d0ffeca457dfe038
Heliex <Christophe.gerard8@gmail.com>
2015-06-15 13:34:13 +0200

    J'ai ajouter  la librairie javax.mail pour pouvoir envoyer un mail sans passer par une appli. Et la j'suis entrain de tester.

--- app/src/main/java/menu/FormulaireSatisfaction.java
+++ app/src/main/java/menu/FormulaireSatisfaction.java
@@ -1,13 +1,17 @@
 package menu;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import barbeasts.plastprod.R;
+import other.GmailSender;
 
 /**
  * Created by christophe on 01/04/2015.
@@ -21,10 +25,21 @@ public class FormulaireSatisfaction extends Fragment {
     {
         View rootView = inflater.inflate(R.layout.fragment_formulaire,container,false);
         final Button envoiForm = (Button) rootView.findViewById(R.id.envoiForm);
+
         envoiForm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                envoiForm.setText("Clicked");
+
+                EditText editText = (EditText) getActivity().findViewById(R.id.editText2);
+                String mail = editText.getText().toString();
+                try {
+                    GmailSender gmail = new GmailSender("christophe.gerard8@gmail.com", "chris88110");
+                    gmail.sendMail("Enquête de satisfaction","Nous vous demandons 5 minutes de votre temps pour consulter ce formulaire : https://docs.google.com/forms/d/1DGX6i1U-1kPehjOEf3uDdkanB6eu07p8gLKvT-YevXY/viewform?usp=send_form","christophe.gerard8@gmail.com",mail);
+                }
+                catch(Exception e)
+                {
+                    Log.e("SendMail", e.getMessage(),e) ;
+                }
             }
         });
         return rootView;
