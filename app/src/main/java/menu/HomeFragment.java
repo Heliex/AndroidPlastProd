163a404b701c6c15e46ee9f591d27858480d9f0f
Heliex <Christophe.gerard8@gmail.com>
2015-07-15 11:18:26 +0200

    Changement de l'écran d'accueil, ajout du bouton de synchro.

--- app/src/main/java/menu/HomeFragment.java
+++ app/src/main/java/menu/HomeFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
